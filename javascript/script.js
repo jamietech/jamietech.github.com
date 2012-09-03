7edf1011813a0693a670bff0a87d7f784e4c8cb1
jamietech <jamie@jamietech.co.cc>
2012-09-03 19:55:21 +1000

    Update and API test.

---
+++ javascript/script.js
@@ -0,0 +1,12 @@
+$(function() {
+    var repos = 0;
+    var url = "https://api.github.com/users/jamietech/repos?sort=pushed&type=owner";
+    $.getJSON(url, function(data) {
+        var $item = $("<li>").addClass("repo grid-1 " + (repo.language || '').toLowerCase());
+        var $link = $("<a>").attr("href", repoUrl(repo)).appendTo($item);
+        $link.append($("<h2>").text(repo.name));
+        $link.append($("<h3>").text(repo.language));
+        $link.append($("<p>").text(repoDescription(repo)));
+        $item.appendTo("#repos");
+    });
+});
